d7427f8e8af636632ca1a22bcc01526df75ac4fd
Mateus <mateusfnogueira@outlook.com>
2020-11-26 12:52:32 -0300

    implementando pagina de login front

---
+++ components/Header.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="header">
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      clipped="true"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar :clipped-left="clipped" fixed app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-btn icon @click.stop="miniVariant = !miniVariant">
+        <v-icon>mdi-{{ `chevron-${miniVariant ? "right" : "left"}` }}</v-icon>
+      </v-btn>
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: true,
+      drawer: true,
+      items: [
+        {
+          icon: "mdi-apps",
+          title: "DashBoard",
+          to: "/"
+        },
+        {
+          icon: "mdi-bank-transfer",
+          title: "Transitions",
+          to: "/Transitions"
+        },
+        {
+          icon: "mdi-account-circle",
+          title: "User",
+          to: "/UserEdit"
+        },
+        {
+          icon: "mdi-logout",
+          title: "Logout",
+          to: "/Login"
+        }
+      ],
+      miniVariant: true,
+      right: true,
+      rightDrawer: false,
+      title: "XBank"
+    };
+  }
+};
+</script>
+
+<style></style>
---
+++ pages/Login.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="content-login justify-center d-flex">
+    <v-card
+      class="card-login align-center d-flex flex-column ma-16 pa-4"
+      width="30%"
+      rounded="xl"
+    >
+      <v-card-title>Acesse sua conta</v-card-title>
+      <v-text-field label="Login" outlined dense></v-text-field>
+      <v-text-field label="Password" outlined dense></v-text-field>
+      <v-btn color="">Submit</v-btn>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.content-login {
+  .card-login {
+    background: linear-gradient(
+      0deg,
+      rgba(2, 0, 36, 1) 0%,
+      rgba(9, 9, 121, 0.9500175070028011) 10%,
+      rgba(0, 212, 255, 1) 90%
+    );
+    .v-text-field {
+      width: 50%;
+    }
+  }
+}
+</style>
